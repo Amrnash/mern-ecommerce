d06ef9e5ec1408c756edbe9c55e8fc12e19dea77
Amr Nashaat <amrnashaat8@gmail.com>
2020-10-03 18:54:41 +0200

    git inialize and frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Home />
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+export default App;
