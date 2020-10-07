23c22de1317709077fea6436b2600f901f651cc5
Amr Nashaat <amrnashaat8@gmail.com>
2020-10-07 18:35:37 +0200

    added a controllers folder that holds the logic for each route

---
+++ backend/controllers/product.js
@@ -0,0 +1,20 @@
+import Product from "../models/Product.js";
+import asyncHandler from "express-async-handler";
+
+// Public GET /api/products------fetch all products
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  return res.json(products);
+});
+// Public GET /api/products/:id------fetch single product
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById };
