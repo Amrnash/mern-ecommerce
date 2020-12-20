a39ac04314359b95e6dcdf5cb942af6f8edbc67a
Amr Nashaat <amrnashaat8@gmail.com>
2020-12-20 04:34:00 +0200

    added update user profile route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { protect } from "../middleware/auth.js";
-import { authUser, getUserProfile, registerUser } from "../controllers/user.js";
+import { authUser, getUserProfile, registerUser, updateUserProfile } from "../controllers/user.js";
 const router = express.Router();
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
