0d78ae41df8698a5a4d91fc31b8a55a88d145a23
Amr Nashaat <amrnashaat8@gmail.com>
2021-01-07 20:56:30 +0200

    added checkout steps component

--- frontend/src/pages/Shipping.jsx
+++ frontend/src/pages/Shipping.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from '../actions/cart';
 
 const Shipping = ({ history }) => {
@@ -18,10 +19,11 @@ const Shipping = ({ history }) => {
     const submitHandler = (e) => {
         e.preventDefault();
         dispatch(saveShippingAddress({ address, city, postalCode, country }));
-        history.push('/payment')
+        history.push('/payment');
     }
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2 />
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId="address">
