8e23c78645ead195daada734487dc7c0ea4fb44c
Amr Nashaat <amrnashaat8@gmail.com>
2021-01-12 16:02:06 +0200

    Added order details reducer & action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer, productDetailsReducer } from "./reducers/product";
 import { cartReducer } from "./reducers/cart";
-import { orderCreateReducer } from "./reducers/order";
+import { orderCreateReducer, orderDetailsReducer } from "./reducers/order";
 import { 
   userLoginReducer, 
   userRegisterReducer, 
@@ -20,7 +20,8 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
-  orderCreate: orderCreateReducer 
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer
 });
 
 // Get Data From Storage
