4ab6c9d2cb21ef75383790e719772992d9600716
Amr Nashaat <amrnashaat8@gmail.com>
2021-01-24 09:26:52 +0200

    product review frontend

--- frontend/src/reducers/product.js
+++ frontend/src/reducers/product.js
@@ -16,6 +16,10 @@ import {
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAIL,
   PRODUCT_UPDATE_RESET,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_CREATE_REVIEW_RESET,
 } from "../types/product";
 const productListInitialState = {
   products: [],
@@ -34,23 +38,18 @@ export const productListReducer = (state = productListInitialState, action) => {
       return state;
   }
 };
-const productDetailsInitialState = {
-  product: {},
-  reviews: [],
-  loading: false,
-  error: null,
-};
+
 export const productDetailsReducer = (
-  state = productDetailsInitialState,
+  state = { product: { reviews: [] } },
   action
 ) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
       return { ...state, loading: true };
     case PRODUCT_DETAILS_SUCCESS:
-      return { ...state, loading: false, product: action.payload };
+      return { loading: false, product: action.payload };
     case PRODUCT_DETAILS_FAIL:
-      return { ...state, loading: false, error: action.payload };
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
@@ -94,6 +93,21 @@ export const productUpdateReducer = (state = { product: {} }, action) => {
       return { loading: false, error: action.payload };
     case PRODUCT_UPDATE_RESET:
       return { product: {} };
+    default:
+      return state;
+  }
+};
+
+export const productReviewCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REVIEW_REQUEST:
+      return { loading: true };
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return { loading: false, success: true };
+    case PRODUCT_CREATE_REVIEW_FAIL:
+      return { loading: false, error: action.payload };
+    case PRODUCT_CREATE_REVIEW_RESET:
+      return {};
     default:
       return state;
   }
